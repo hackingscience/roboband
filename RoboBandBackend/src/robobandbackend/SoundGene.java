ed02cfb1304619068dc514f5f68e0291ff25330a
tjb5215 <tjb5215@psu.edu>
2013-01-21 12:57:09 -0500

    JCollider Addon

---
+++ RoboBandBackend/src/robobandbackend/SoundGene.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robobandbackend;
+
+/**
+ *
+ * @author toddbodnar
+ */
+public class SoundGene {
+    public SoundGene()
+    {
+        
+    }
+    
+}
